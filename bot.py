008cd5f4205ccac981bf6735fc684c9c53ab6937
joonsauce <77248354+joonsauce@users.noreply.github.com>
2024-01-04 19:47:35 -0500

    Feature: Users can be removed based on the roles they have

--- bot.py
+++ bot.py
@@ -2,11 +2,27 @@ from help import *
 from settings import *
 
 @bot.command()
-async def reset(ctx):
+async def reset(ctx, *, message=''):
+    if not ctx.author.guild_permissions.administrator or ctx.author.bot:
+        await ctx.send("You do not have the permission to perform this command.")
+        return
+
+    roles = []
+
+    if message:
+        roles = message.split(",")
+        roles = [x.replace(" ", "").replace("<", "").replace(">", "").replace("&", "").replace("@", "") for x in roles]
+        guild_roles = [str(r.id) for r in ctx.guild.roles]
+        for i in roles:
+            if i not in guild_roles:
+                await ctx.send("Invalid role selected. Please try again.")
+                return
+        roles = [discord.utils.get(ctx.guild.roles, id=int(x)) for x in roles]
+
     await ctx.send("Are you sure you want to remove ALL non-admin users from this server?\n"
                    "This action CANNOT be undone.\n"
                    "If you are sure, please respond with the name of this server.\n"
-                   "Note that name capitalization matters.")
+                   "Please note that name capitalization matters.")
 
     def checkMessage(msg):
         return msg.author == ctx.author
@@ -17,11 +33,18 @@ async def reset(ctx):
         await ctx.send("Timed out. Please try again.")
         return
 
-    if msg.content == ctx.message.guild.name:
+    if not msg.content == ctx.message.guild.name:
+        await ctx.send("The server name is incorrect. Please try again.")
+        return
+
+    if roles:
+        for role in roles:
+            for m in role.members:
+                if not m.guild_permissions.administrator:
+                    await m.kick()
+    else:
         for m in ctx.message.guild.members:
             if not m.guild_permissions.administrator:
                 await m.kick()
-    else:
-        await ctx.send("The server name is incorrect. Please try again.")
 
 bot.run(bot_token)
